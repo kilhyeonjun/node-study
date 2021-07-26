509eed3a80a3e5eb3998e5dbb64f7af10a242965
길현준 <hyeonjunkil@ohcomon.com>
2021-07-26 11:29:48 +0900

    todotemplate 구성

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
+import TodoListTemplate from './components/TodoListTemplate';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        App
-      </div>
-    );
-  }
+    render() {
+        return <TodoListTemplate>템플릿 완성</TodoListTemplate>;
+    }
 }
 
 export default App;
