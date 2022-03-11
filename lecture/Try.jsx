63250df810175ccf1e5d3f8afc8613ea534167d8
kilhyeonjun <kboxstar@gmail.com>
2022-03-11 21:06:56 +0900

    숫자야구

--- lecture/Try.jsx
+++ lecture/Try.jsx
@@ -1,17 +1,14 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 class Try extends Component {
-	render() {
-		return (
-			<li>
-				<b>{this.props.value.fruit}</b> - {this.props.index}
-				<div>컨텐츠</div>
-				<div>컨텐츠1</div>
-				<div>컨텐츠2</div>
-				<div>컨텐츠3</div>
-			</li>
-		);
-	}
+  render() {
+    return (
+      <li>
+        <div>{this.props.tryInfo.try}</div>
+        <div>{this.props.tryInfo.result}</div>
+      </li>
+    );
+  }
 }
 
 export default Try;
