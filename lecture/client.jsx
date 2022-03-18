352c8b9db84a4913f5f1fd3b8c286520dd27449b
kilhyeonjun <kboxstar@gmail.com>
2022-03-18 21:15:34 +0900

    로또 추첨기

--- lecture/client.jsx
+++ lecture/client.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { hot } from 'react-hot-loader/root';
 
-import RSP from './RSP';
-const Hot = hot(RSP);
+import Lotto from './Lotto';
+const Hot = hot(Lotto);
 ReactDom.render(<Hot />, document.querySelector('#root'));
