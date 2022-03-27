f9c3c2ea90a993e19eadb05285416e04005462ba
kilhyeonjun <kboxstar@gmail.com>
2022-03-27 16:25:56 +0900

    Insert Loggin Function with dummyData

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
 import {Menu, Input, Row, Col} from 'antd';
+import styled from 'styled-components';
+
+import UserProfile from '../components/UserProfile';
+import LoginForm from '../components/LoginForm';
+
+const SearchInput = styled(Input.Search)`
+  vertical-align: middle;
+`;
 
 const AppLayout = ({children}) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
     <div>
       <Menu mode="horizontal">
@@ -18,7 +28,7 @@ const AppLayout = ({children}) => {
           </Link>
         </Menu.Item>
         <Menu.Item>
-          <Input.Search enterButton style={{verticalAlign: 'middle'}} />
+          <SearchInput enterButton />
         </Menu.Item>
         <Menu.Item>
           <Link href="/signup">
@@ -27,7 +37,9 @@ const AppLayout = ({children}) => {
         </Menu.Item>
       </Menu>
       <Row gutter={8}>
-        <Col xs={24} md={6}></Col>
+        <Col xs={24} md={6}>
+          {isLoggedIn ? <UserProfile setIsLoggedIn={setIsLoggedIn} /> : <LoginForm setIsLoggedIn={setIsLoggedIn} />}
+        </Col>
         <Col xs={24} md={12}></Col>
         <Col xs={24} md={6}>
           <a href="https://github.com/kilhyeonjun/react-nodebird" target="_blank" rel="noreferrer noopener">
