bc2ce44cdac81031ff9d7d342a049427171b1255
kilhyeonjun <kboxstar@gmail.com>
2022-06-16 11:11:22 +0900

    saga reducer 연결

--- prepare/front/components/LoginForm.js
+++ prepare/front/components/LoginForm.js
@@ -2,9 +2,9 @@ import React, {useCallback} from 'react';
 import {Button, Form, Input} from 'antd';
 import Link from 'next/link';
 import styled from 'styled-components';
-import {useDispatch} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import useInput from '../hooks/useInput';
-import {loginAction} from '../reducers/user';
+import { loginRequestAction } from '../reducers/user';
 
 const ButtonWrapper = styled.div`
   margin-top: 10px;
@@ -16,12 +16,13 @@ const FormWrapper = styled(Form)`
 
 const LoginForm = () => {
   const dispatch = useDispatch();
+  const { isLoggingIn } = useSelector((state) => state.user)
   const [id, onChangeId] = useInput('');
   const [password, onChangePassword] = useInput('');
 
   const onSubmitForm = useCallback(() => {
     console.log(id, password);
-    dispatch(loginAction({id, password}));
+    dispatch(loginRequestAction({id, password}));
   }, [id, password]);
 
   return (
@@ -37,7 +38,7 @@ const LoginForm = () => {
         <Input name="user-password" type="password" value={password} onChange={onChangePassword} required />
       </div>
       <ButtonWrapper>
-        <Button type="primary" htmlType="submit" loading={false}>
+        <Button type="primary" htmlType="submit" loading={isLoggingIn}>
           로그인
         </Button>
         <Link href="/signup">
