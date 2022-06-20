318648407b9e49883a273807374e1347ddc67973
kilhyeonjun <kboxstar@gmail.com>
2022-06-20 09:08:36 +0900

    express 프레임워크 적용 및 라우터 분리

---
+++ prepare/back/routes/post.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const router = express.Router();
+
+app.post('/', (req, res) => {
+    res.json({ id: 1, content: 'hello' });
+});
+
+app.delete('/', (req, res) => {
+    res.json({ id: 1 });
+});
+
+module.exports = router;
