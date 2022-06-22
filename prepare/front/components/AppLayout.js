4cd70be554a0d120e1f27ff587798d83ae709341
kilhyeonjun <kboxstar@gmail.com>
2022-06-23 00:17:56 +0900

    add Menu.Item key

--- prepare/front/components/AppLayout.js
+++ prepare/front/components/AppLayout.js
@@ -34,20 +34,20 @@ const AppLayout = ({children}) => {
     <div>
       <Global />
       <Menu mode="horizontal">
-        <Menu.Item>
+        <Menu.Item key="home">
           <Link href="/">
             <a>HOME</a>
           </Link>
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key="profile">
           <Link href="/profile">
             <a>프로필</a>
           </Link>
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key="search">
           <SearchInput enterButton />
         </Menu.Item>
-        <Menu.Item>
+        <Menu.Item key="signup">
           <Link href="/signup">
             <a>회원가입</a>
           </Link>
