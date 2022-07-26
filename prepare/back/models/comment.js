d1265524be7741d32eaad97c7bb94ba62cd28ada
kilhyeonjun <kboxstar@gmail.com>
2022-07-26 17:20:42 +0900

    sequelize model 최신 문법

--- prepare/back/models/comment.js
+++ prepare/back/models/comment.js
@@ -1,24 +1,30 @@
-module.exports = (sequelize, DataTypes) => {
-  const Comment = sequelize.define(
-    'Comment',
-    {
-      // MySQL에는 comments 테이블 생성
-      // id가 기본적으로 들어있다.
-      content: {
-        type: DataTypes.TEXT,
-        allowNull: false,
+const DataTypes = require('sequelize');
+const {Model} = DataTypes;
+
+module.exports = class Comment extends Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        // id가 기본적으로 들어있다.
+        content: {
+          type: DataTypes.TEXT,
+          allowNull: false,
+        },
+        // UserId: 1
+        // PostId: 3
       },
-    },
-    // UserId: {}
-    // PostId: {}
-    {
-      charset: 'utf8mb4',
-      collate: 'utf8mb4_general_ci', // 이모티콘 저장
-    },
-  );
-  Comment.associate = (db) => {
+      {
+        modelName: 'Comment',
+        tableName: 'comments',
+        charset: 'utf8mb4',
+        collate: 'utf8mb4_general_ci', // 이모티콘 저장
+        sequelize,
+      },
+    );
+  }
+
+  static associate(db) {
     db.Comment.belongsTo(db.User);
     db.Comment.belongsTo(db.Post);
-  };
-  return Comment;
+  }
 };
