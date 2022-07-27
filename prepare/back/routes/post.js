486b0d7b7fce3fc7465fc77a5426ef4c3ed883e1
kilhyeonjun <kboxstar@gmail.com>
2022-07-27 15:44:58 +0900

    back apply img upload aws s3

--- prepare/back/routes/post.js
+++ prepare/back/routes/post.js
@@ -2,6 +2,8 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const multerS3 = require('multer-s3');
+const AWS = require('aws-sdk');
 
 const {Post, Image, Comment, User, Hashtag} = require('../models');
 const {isLoggedIn} = require('./middlewares');
@@ -15,18 +17,20 @@ try {
   fs.mkdirSync('uploads');
 }
 
+AWS.config.update({
+  accessKeyId: process.env.S3_ACCESS_KEY_ID,
+  secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
+  region: 'us-east-1',
+});
 const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, done) {
-      done(null, 'uploads');
-    },
-    filename(req, file, done) {
-      const ext = path.extname(file.originalname); // 확장자 추출
-      const basename = path.basename(file.originalname, ext);
-      done(null, basename + '_' + new Date().getTime() + ext);
+  storage: multerS3({
+    s3: new AWS.S3(),
+    bucket: 'kilhyeonjun',
+    key(req, file, cb) {
+      cb(null, `original/${Date.now()}_${path.basename(file.originalname)}`);
     },
   }),
-  limits: {filename: 20 * 1024 * 1024}, // 20MB
+  limits: {fileSize: 20 * 1024 * 1024}, // 20MB
 });
 
 router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
@@ -89,7 +93,7 @@ router.post('/', isLoggedIn, upload.none(), async (req, res, next) => {
 });
 
 router.post('/images', isLoggedIn, upload.array('image'), async (req, res, next) => {
-  res.json(req.files.map((v) => v.filename));
+  res.json(req.files.map((v) => v.location.replace(/\/original\//, '/thumb/')));
 });
 
 router.get('/:postId', async (req, res, next) => {
