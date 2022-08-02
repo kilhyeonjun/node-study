49ba7f1c40aa8cdfaf8031b1bee33824709197de
kilhyeonjun <kboxstar@gmail.com>
2022-08-02 14:16:00 +0900

    swagger api 문서 생성

--- src/users/dto/join.request.dto.ts
+++ src/users/dto/join.request.dto.ts
@@ -1,5 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class JoinRequestDto {
+  @ApiProperty({
+    example: 'kboxstar@gmail.com',
+    description: '이메일',
+    required: true,
+  })
   public email: string;
+
+  @ApiProperty({
+    example: '펭귄',
+    description: '닉네임',
+    required: true,
+  })
   public nickname: string;
+
+  @ApiProperty({
+    example: '1234',
+    description: '비밀번호',
+    required: true,
+  })
   public password: string;
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,27 +1,50 @@
 import { Body, Controller, Get, Post, Req, Res } from '@nestjs/common';
+import {
+  ApiOkResponse,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 import { JoinRequestDto } from './dto/join.request.dto';
 import { UsersService } from './users.service';
+import { UserDto } from '../common/dto/user.dto';
 
+@ApiTags('USER')
 @Controller('api/users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @ApiResponse({
+    type: UserDto,
+  })
+  @ApiOperation({ summary: '내 정보 조회' })
   @Get()
   getUsers(@Req() req) {
     return req.user;
   }
 
+  @ApiOperation({ summary: '회원가입' })
   @Post()
   postUsers(@Body() data: JoinRequestDto) {
     this.usersService.postUsers(data.email, data.nickname, data.password);
     return;
   }
 
+  @ApiOkResponse({
+    description: '성공',
+    type: UserDto,
+  })
+  @ApiResponse({
+    status: 500,
+    description: '서버 에러',
+  })
+  @ApiOperation({ summary: '로그인' })
   @Post('login')
   login(@Req() req) {
     return req.user;
   }
 
+  @ApiOperation({ summary: '로그아웃' })
   @Post('logout')
   logout(@Req() req, @Res() res) {
     req.logOut();
