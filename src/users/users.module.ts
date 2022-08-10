3ebfa602a0203a87c37f90c1b4e8d142573a5308
kilhyeonjun <kboxstar@gmail.com>
2022-08-10 17:25:50 +0900

    add join fn transaction

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Users } from '../entities/Users';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
+import { WorkspaceMembers } from '../entities/WorkspaceMembers';
+import { ChannelMembers } from '../entities/ChannelMembers';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users])],
+  imports: [
+    TypeOrmModule.forFeature([Users, WorkspaceMembers, ChannelMembers]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,61 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import { Users } from '../entities/Users';
 import bcrypt from 'bcrypt';
+import { WorkspaceMembers } from '../entities/WorkspaceMembers';
+import { ChannelMembers } from '../entities/ChannelMembers';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(Users) private usersRepository: Repository<Users>,
+    @InjectRepository(WorkspaceMembers)
+    private workspaceMembersRepository: Repository<WorkspaceMembers>,
+    @InjectRepository(ChannelMembers)
+    private channelMembersRepository: Repository<ChannelMembers>,
+    private connection: Connection,
   ) {}
+
   async join(email: string, nickname: string, password: string) {
-    const user = await this.usersRepository.findOne({
+    const queryRunner = this.connection.createQueryRunner();
+    await queryRunner.connect();
+    await queryRunner.startTransaction();
+    const user = await queryRunner.manager.getRepository(Users).findOne({
       where: { email: email },
     });
 
     if (user) throw new UnauthorizedException('이미 존재하는 사용자 입니다.');
 
     const hashedPassword = await bcrypt.hash(password, 12);
-    await this.usersRepository.save({
-      email,
-      nickname,
-      password: hashedPassword,
-    });
+
+    try {
+      const returned = await queryRunner.manager.getRepository(Users).save({
+        email,
+        nickname,
+        password: hashedPassword,
+      });
+
+      const workspaceMembers = queryRunner.manager
+        .getRepository(WorkspaceMembers)
+        .create();
+      workspaceMembers.UserId = returned.id;
+      workspaceMembers.WorkspaceId = 1;
+      await queryRunner.manager
+        .getRepository(WorkspaceMembers)
+        .save(workspaceMembers);
+
+      await queryRunner.manager.getRepository(ChannelMembers).save({
+        UserId: returned.id,
+        ChannelId: 1,
+      });
+      return true;
+    } catch (error) {
+      console.error(error);
+      await queryRunner.rollbackTransaction();
+      throw error;
+    } finally {
+      await queryRunner.release();
+    }
   }
 }
