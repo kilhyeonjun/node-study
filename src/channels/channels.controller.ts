c77f8fe86ddfa97f35fefb2533451a26688ce366
kilhyeonjun <kboxstar@gmail.com>
2022-10-04 15:00:21 +0900

    add channels api

--- src/channels/channels.controller.ts
+++ src/channels/channels.controller.ts
@@ -1,11 +1,18 @@
 import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
+import { User } from '../common/decorators/user.decorator';
+import { ChannelsService } from './channels.service';
+import { Users } from '../entities/Users';
+import { PostChatDto } from './dto/post-chat.dto';
 
 @ApiTags('CHANNEL')
 @Controller('api/workspaces/:url/channels')
 export class ChannelsController {
+  constructor(private channelsService: ChannelsService) {}
   @Get()
-  getAllChannels() {}
+  getAllChannels(@Param('url') url: string, @User() user) {
+    return this.channelsService.getWorkspaceChannels(url, user.id);
+  }
 
   @Post()
   createChannels() {}
@@ -14,16 +21,53 @@ export class ChannelsController {
   getSpecificChannel() {}
 
   @Get(':name/chats')
-  getChats(@Query() query, @Param() param) {
+  getChats(
+    @Param('url') url: string,
+    @Param('name') name: string,
+    @Query() query,
+    @Param() param,
+  ) {
     console.log(query.perPage, query.Page);
     console.log(param.id, param.url);
+    return this.channelsService.getWorkspaceChannelChats(
+      url,
+      name,
+      query.perPage,
+      query.page,
+    );
   }
 
   @Post(':name/chats')
-  postChat(@Body() body) {}
+  async postChat(
+    @Param('url') url,
+    @Param('name') name,
+    @Body() body: PostChatDto,
+    @User() user: Users,
+  ) {
+    return this.channelsService.postChat({
+      url,
+      name,
+      content: body.content,
+      myId: user.id,
+    });
+  }
+
+  @Post(':name/images')
+  postImages(@Body() body) {}
+
+  @Get(':name/unreads')
+  getUnreads(
+    @Param('url') url: string,
+    @Param('name') name: string,
+    @Query('after') after: string,
+  ) {
+    return this.channelsService.getChannelUnreadsCount(url, name, after);
+  }
 
   @Get(':name/members')
-  getAllMembers() {}
+  getAllMembers(@Param('url') url: string, @Param('name') name: string) {
+    return this.channelsService.getWorkspaceChannelMembers(url, name);
+  }
 
   @Post(':name/members')
   inviteMembers() {}
--- src/channels/channels.module.ts
+++ src/channels/channels.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { ChannelsController } from './channels.controller';
 import { ChannelsService } from './channels.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ChannelChats } from '../entities/ChannelChats';
+import { ChannelMembers } from '../entities/ChannelMembers';
+import { Channels } from '../entities/Channels';
+import { Users } from '../entities/Users';
+import { Workspaces } from '../entities/Workspaces';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Channels,
+      ChannelChats,
+      Users,
+      Workspaces,
+      ChannelMembers,
+    ]),
+  ],
   controllers: [ChannelsController],
-  providers: [ChannelsService]
+  providers: [ChannelsService],
 })
 export class ChannelsModule {}
