25b80795d41ed2af4ed10f12b89eb4e0be742265
kilhyeonjun <kboxstar@gmail.com>
2023-01-18 22:15:29 +0900

    add: swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Auth API')
+    .setDescription('auth 개발 문서')
+    .setVersion('1.0')
+    .addCookieAuth('connect.sid')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   const port = process.env.PORT || 8060;
   await app.listen(port);
   console.log(`Server is running at http://localhost:${port}...`);
